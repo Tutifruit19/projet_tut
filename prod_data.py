3b7ddc8582b3b2c7e5103fe5480f60cbf5243f74
Tutifruit19 <64989198+Tutifruit19@users.noreply.github.com>
2022-10-26 16:50:26 +0200

    creation graph plotly

--- prod_data.py
+++ prod_data.py
@@ -4,7 +4,12 @@ from datetime import timedelta
 import xarray as xr
 import numpy as np
 import os
+import plotly.graph_objs as go
 
+date_debut_ARO = '2022101215'
+echeance_ARO = '2022101409'
+date_debut_ARP= '2022101218'
+echeance_ARP = '2022101406'
 
 def convert(str_time):
     format = '%Y%m%d%H'
@@ -23,21 +28,22 @@ def recovery_data_aro(echeance,run_debut,param,hauteur,membre):
     delta = timedelta(hours=6)
     t = convert(run_debut)
     difference = diff(echeance,run_debut)
-    list_timerun = [t.strftime('%Y%m%d%H')]
+    list_timerun_aro = [t.strftime('%Y%m%d%H')]
     list_echeance = [difference]
     while t < convert(echeance):
         t = t + delta
         difference = difference-6
-        list_timerun.append(t.strftime('%Y%m%d%H'))
+        list_timerun_aro.append(t.strftime('%Y%m%d%H'))
         list_echeance.append(difference)
-    list_timerun.pop()
+    list_timerun_aro.pop()
     list_echeance.pop()
-    if len(list_timerun) == len(list_echeance):
-        for i in range(len(list_timerun)):
-            run = list_timerun[i]
+    if len(list_timerun_aro) == len(list_echeance):
+        for i in range(len(list_timerun_aro)):
+            run = list_timerun_aro[i]
             profondeur = list_echeance[i]
             os.system("./recupPrevisionAROME_hauteur.sh "+run+" "+str(profondeur)+" 2 T "+membre+" /home/mpma/henona/recovery_data/"+path)
-            os.system("grib_to_netcdf /home/mpma/henona/recovery_data/"+path+"/Prevision-"+param+"-"+hauteur+"-m-EURW1S40-"+list_timerun[i]+"-ECH-"+str(list_echeance[i])+".grb -o /home/mpma/henona/recovery_data/"+path+"/Prevision-"+param+"-"+hauteur+"-m-EURW1S40-"+list_timerun[i]+"-ECH-"+str(list_echeance[i])+".nc")
+            os.system("grib_to_netcdf /home/mpma/henona/recovery_data/"+path+"/Prevision-"+param+"-"+hauteur+"-m-EURW1S40-"+list_timerun_aro[i]+"-ECH-"+str(list_echeance[i])+".grb -o /home/mpma/henona/recovery_data/"+path+"/Prevision-"+param+"-"+hauteur+"-m-EURW1S40-"+list_timerun_aro[i]+"-ECH-"+str(list_echeance[i])+".nc")
+    return list_timerun_aro
 
 def recovery_data_arp(echeance,run_debut,param,hauteur,membre):
     os.system("mkdir PE_"+membre+str(run_debut)+"_"+str(echeance)+"_"+str(param)+"_"+str(hauteur))
@@ -46,22 +52,22 @@ def recovery_data_arp(echeance,run_debut,param,hauteur,membre):
     delta = timedelta(hours=6)
     t = convert(run_debut)
     difference = diff(echeance,run_debut)
-    list_timerun = [t.strftime('%Y%m%d%H')]
+    list_timerun_arp = [t.strftime('%Y%m%d%H')]
     list_echeance = [difference]
     while t < convert(echeance):
         t = t + delta
         difference = difference-6
-        list_timerun.append(t.strftime('%Y%m%d%H'))
+        list_timerun_arp.append(t.strftime('%Y%m%d%H'))
         list_echeance.append(difference)
-    list_timerun.pop()
+    list_timerun_arp.pop()
     list_echeance.pop()
-    if len(list_timerun) == len(list_echeance):
-        for i in range(len(list_timerun)):
-            run = list_timerun[i]
+    if len(list_timerun_arp) == len(list_echeance):
+        for i in range(len(list_timerun_arp)):
+            run = list_timerun_arp[i]
             profondeur = list_echeance[i]
             os.system("./recupPrevisionARPEGE_hauteur.sh "+run+" "+str(profondeur)+" 2 T "+membre+" /home/mpma/henona/recovery_data/"+path)
-            os.system("grib_to_netcdf /home/mpma/henona/recovery_data/"+path+"/Prevision-"+param+"-"+hauteur+"-m-EURAT01-"+list_timerun[i]+"-ECH-"+str(list_echeance[i])+".grb -o /home/mpma/henona/recovery_data/"+path+"/Prevision-"+param+"-"+hauteur+"-m-EURAT01-"+list_timerun[i]+"-ECH-"+str(list_echeance[i])+".nc")
-
+            os.system("grib_to_netcdf /home/mpma/henona/recovery_data/"+path+"/Prevision-"+param+"-"+hauteur+"-m-EURAT01-"+list_timerun_arp[i]+"-ECH-"+str(list_echeance[i])+".grb -o /home/mpma/henona/recovery_data/"+path+"/Prevision-"+param+"-"+hauteur+"-m-EURAT01-"+list_timerun_arp[i]+"-ECH-"+str(list_echeance[i])+".nc")
+    return list_timerun_arp
 def extraction_pt_grille(lat,lon,filename,path,cible):
     #Toulouse lat=43.5866 et lon=1.4605
     os.system("cdo -remapnn,lon="+lon+"/lat="+lat+" "+path+" "+cible+"/remap_"+filename)
@@ -70,11 +76,11 @@ def extraction_pt_grille(lat,lon,filename,path,cible):
 
 membres_PEARO = ["PEAROME000","PEAROME001","PEAROME002","PEAROME003","PEAROME004","PEAROME005","PEAROME006","PEAROME007","PEAROME008","PEAROME009","PEAROME010","PEAROME011","PEAROME012","PEAROME013","PEAROME014","PEAROME015","PEAROME016"]
 for x in membres_PEARO:
-    recovery_data_aro("2022101409","2022101215","T","2",x)
+    list_timerun_aro = recovery_data_aro(echeance_ARO,date_debut_ARO,"T","2",x)
 
 membres_ARP = ["PEARP000","PEARP001","PEARP002","PEARP003","PEARP004","PEARP005","PEARP006","PEARP007","PEARP008","PEARP009","PEARP010","PEARP011","PEARP012","PEARP013","PEARP014","PEARP015","PEARP016","PEARP017","PEARP018","PEARP019","PEARP020","PEARP021","PEARP022","PEARP023","PEARP024","PEARP025","PEARP026","PEARP027","PEARP028","PEARP029","PEARP030","PEARP031","PEARP032","PEARP033","PEARP034"]
 for x in membres_ARP:
-    recovery_data_arp("2022101406","2022101218","T","2",x)
+    list_timerun_arp = recovery_data_arp(echeance_ARP,date_debut_ARP,"T","2",x)
 
 os.system("mkdir temporaire")
 list_files = os.listdir("/home/mpma/henona/recovery_data")
@@ -156,3 +162,52 @@ making_output(output_ARO[2],"/home/mpma/henona/Q_90_ARO.txt")
 making_output(output_ARP[0],"/home/mpma/henona/run_determiste_ARP.txt")
 making_output(output_ARP[1],"/home/mpma/henona/Q_10_ARP.txt")
 making_output(output_ARP[2],"/home/mpma/henona/Q_90_ARP.txt")
+
+
+
+
+"""Q_10_ARP = np.genfromtxt('/home/mpma/henona/Q_10_ARP.txt')
+run_deterministe_ARP = np.genfromtxt('/home/mpma/henona/run_determiste_ARP.txt')
+Q_90_ARP = np.genfromtxt('/home/mpma/henona/Q_10_ARP.txt')
+Q_10_ARO = np.genfromtxt('/home/mpma/henona/Q_10_ARO.txt')
+run_deterministe_ARO = np.genfromtxt('/home/mpma/henona/run_determiste_ARO.txt')
+Q_90_ARO = np.genfromtxt('/home/mpma/henona/Q_90_ARO.txt')
+
+
+
+fig = go.Figure([
+    go.Scatter(
+        name='run deterministe',
+        x=list_timerun_aro,
+        y=run_deterministe_ARO,
+        mode='lines',
+        line=dict(color='rgb(31, 119, 180)'),
+    ),
+    go.Scatter(
+        name='Q 90',
+        x=list_timerun_aro,
+        y=Q_90_ARO,
+        mode='lines',
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        showlegend=False
+    ),
+    go.Scatter(
+        name='Q_10',
+        x=list_timerun_aro,
+        y=Q_10_ARO,
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        mode='lines',
+        fillcolor='rgba(68, 68, 68, 0.3)',
+        fill='tonexty',
+        showlegend=False
+    )
+])
+fig.update_layout(
+    yaxis_title='temperature en Kelvin',
+    title='Evolution des runs AROME',
+    hovermode="x"
+)
+fig.show()
+fig.write_html('/home/mpma/henona/graph_test.html')"""
